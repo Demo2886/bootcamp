32d5405d590465090c8a064ad24e8426662330ea
Alexandr Nefedzin <Demo2886@yandex.by>
2023-03-22 13:18:21 +0000

    pipeline

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 # entrypoint.sh
 
 # Run migrations
-python manage.py migrate
-python manage.py runserver
+python ./manage.py migrate
+python ./manage.py runserver
 # Then run the main container command (passed to us as arguments)
 exec "$@"
